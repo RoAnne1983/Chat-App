2884ef75d0e5587a8018e6d53b5c052f94fdceae
Anne <rocheanne83@gmail.com>
2019-01-09 23:29:09 +0800

    Added button,label and outlets

--- Chat-App/ChatVC.swift
+++ Chat-App/ChatVC.swift
@@ -9,11 +9,19 @@
 import UIKit
 
 class ChatVC: UIViewController {
+    
+    //Outlets
+    
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +30,5 @@ class ChatVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }

a6e654ce20e0fb89d8ad6db1d073928974d12070
Anne <rocheanne83@gmail.com>
2019-01-10 00:17:18 +0800

    Added SWRevealViewController action to menuBtn

--- Chat-App/ChatVC.swift
+++ Chat-App/ChatVC.swift
@@ -18,6 +18,7 @@ class ChatVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController, action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
 
         
         

5cdceabc20737d2137d16a89421817d99ef2d351
Anne <rocheanne83@gmail.com>
2019-01-12 18:39:55 +0800

    Added outlets and actions to LoginVC

--- Chat-App/ChannelVC.swift
+++ Chat-App/ChannelVC.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ChannelVC: UIViewController {
+    
+    //Outlets
+    
+    
+    @IBOutlet weak var loginBtn: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +24,8 @@ class ChannelVC: UIViewController {
 
    
 
+    @IBAction func loginBtnPressed(_ sender: Any) {
+    }
     
 
 }
